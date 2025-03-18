f3b21a41057f8e37a319582fdddeef630ee53b8a
Aditya Suresh Sharma <adityasharma0431@gmail.com>
2025-03-18 19:56:25 +0530

    hero section debug

--- src/components/HeroComponent.jsx
+++ src/components/HeroComponent.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import heroCompBG from "../assets/herocompbg.png"; // Adjust the path as needed
 
 const HeroComponent = ({ title, link, linkText, current }) => {
   return (
     <div className="hero-component-section">
+      <img
+        src={heroCompBG}
+        alt="Hero Background"
+        className="hero-component-image"
+      />
       <div className="hero-component-overlay">
         <h1 className="hero-component-title">{title}</h1>
         <div className="hero-component-breadcrumb">
